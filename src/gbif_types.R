716e221032dca64d65eea71f42f848909d96a8ef
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2023-09-04 14:46:42 +0200

    ipni

--- src/gbif_types.R
+++ src/gbif_types.R
@@ -1,9 +1,14 @@
 library(tidyverse)
 library(magrittr)
 
-cols = readLines("data/colnames.txt")
+cols = readLines("data/colnames_min.txt")
 
 gt = read_tsv("data/0001074-230828120925497/occurrence.txt",
               quote="",
               col_select = all_of(cols),
               col_types = cols(.default = "c"))
+
+ipni = read_tsv("data/00ipni/TypeMaterial.tsv")
+ipnin = read_tsv("data/00ipni/Name.tsv")
+
+ipni = left_join(ipni,ipnin,by=c("col:nameID"="col:ID"))
