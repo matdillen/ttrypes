b161604e42d2e24046810d89da415471cb9f0909
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2024-01-01 14:04:01 +0100

    add docs

--- src/gbif_types.R
+++ src/gbif_types.R
@@ -12,3 +12,25 @@ ipni = read_tsv("data/00ipni/TypeMaterial.tsv")
 ipnin = read_tsv("data/00ipni/Name.tsv")
 
 ipni = left_join(ipni,ipnin,by=c("col:nameID"="col:ID"))
+
+ipni %<>%
+  mutate(fullname = paste(`col:scientificName`,
+                          `col:authorship`))
+
+gt_ipni = gt %>%
+  filter(scientificName%in%ipni$fullname)
+gt_ipni_c = gt_ipni %>%
+  count(scientificName)
+
+gt_max= gt %>%
+  filter(scientificName == "Amorimia pellegrinii R.F.Almeida")
+
+ipni_c = ipni %>%
+  count(`col:citation`)
+
+ipni_c_status = ipni %>%
+  count(`col:status.x`)
+
+ipni_c %>% filter(!is.na(`col:citation`)) %>% pull(n) %>% sum()
+
+gt %>% count(basisOfRecord) %>% mutate(perc = n/sum(n))
